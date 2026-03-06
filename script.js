5f4e656c1cd84ff3635afbe283f72fd67940d421
Nazrul Nazir <nazrul6290@gmail.com>
2026-03-06 21:32:49 +0600

    create login form

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('form-submit')
+.addEventListener('click', function(e){
+    e.preventDefault();
+    const userName = document.getElementById('user-name').value;
+    const password = document.getElementById('password').value;
+    console.log(userName, password)
+
+    if(userName === 'admin' && password === 'admin123'){
+        window.location = "githubProject.html";
+    }else{
+        alert('Wrong user name and password..')
+    }
+
+})
